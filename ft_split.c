b976c954340639bc26a4d54eec4e50f386621b5b
Jordan King yen hii <jking-ye@u90z04s03.42kl.edu.my>
2021-07-29 10:59:43 +0800

    ft_split v3

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jking-ye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 14:18:33 by jking-ye          #+#    #+#             */
-/*   Updated: 2021/07/28 18:22:12 by jking-ye         ###   ########.fr       */
+/*   Updated: 2021/07/29 10:58:19 by jking-ye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,16 @@ static	int	wordcount(const char *s, char c)
 	return (count);
 }
 
+static int wordlen(const char *str, char c)
+{
+	int i;
+
+	i = 0;
+	while (str[i + 1] != c && str[i + 1] != '\0')
+		i++;
+	return (i);
+}
+
 char	**ft_split(const char *s, char c)
 {
 	int		i;
@@ -44,6 +54,8 @@ char	**ft_split(const char *s, char c)
 
 	i = 0;
 	k = 0;
+	if (!s)
+		return (0);
 	ptr = ft_calloc((wordcount(s, c) + 1), 8);
 	if (!ptr)
 		return (0);
@@ -51,9 +63,7 @@ char	**ft_split(const char *s, char c)
 	{
 		if (s[i] != c)
 		{
-			j = 0;
-			while (s[i + j + 1] != c && s[i + j + 1] != '\0')
-				j++;
+			j = wordlen(s + i, c);
 			i = i + j;
 			ptr[k] = malloc(j);
 			ft_strlcpy(ptr[k++], &(s[i - j]), j + 2);

484644e7b4e679daefc480e3a429cdbbab0f9c56
Jordan King yen hii <jking-ye@u90z04s03.42kl.edu.my>
2021-07-29 11:22:41 +0800

    final ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jking-ye <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 14:18:33 by jking-ye          #+#    #+#             */
-/*   Updated: 2021/07/29 11:03:48 by jking-ye         ###   ########.fr       */
+/*   Updated: 2021/07/29 11:21:29 by jking-ye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,6 @@ static	int	wordcount(const char *s, char c)
 	return (count);
 }
 
-static	int	wordlen(const char *str, char c)
-{
-	int	i;
-
-	i = 0;
-	while (str[i + 1] != c && str[i + 1] != '\0')
-		i++;
-	return (i);
-}
-
 char	**ft_split(const char *s, char c)
 {
 	int		i;
@@ -63,10 +53,11 @@ char	**ft_split(const char *s, char c)
 	{
 		if (s[i] != c)
 		{
-			j = wordlen(s + i, c);
+			j = 0;
+			while (s[i + j + 1] != c && s[i + j + 1] != 0)
+				j++;
+			ptr[k++] = ft_substr(s, i, j + 1);
 			i = i + j;
-			ptr[k] = malloc(j);
-			ft_strlcpy(ptr[k++], &(s[i - j]), j + 2);
 		}
 		i++;
 	}
